0529cc780d817780226863e38e3b5e5055b66953
numaresplandy <numa.resplandy@gmail.com>
2020-09-28 11:44:06 +0000

    up

--- agent.py
+++ agent.py
@@ -10,7 +10,7 @@ from tensorflow.keras.optimizers import Adam
 def dq_network(nb_actions,inputs_dim,hidden_size=64,hidden_layers=2): 
     i = Input(shape=(inputs_dim,))
     x=i 
-    x = Dense(32,activation='relu')(x)
+    x = Dense(16,activation='relu')(x)
     x = Dense(16,activation='relu')(x)
     x=Dense(nb_actions)(x)
     model = Model(i,x)
@@ -25,9 +25,9 @@ class agent():
         self.reward=0 #Commun
         self.filename=fname #Commun
         self.size=size
-        self.epsilon=0
+        self.epsilon=1
         self.eps_dec=0.0001 #Commun
-        self.eps_min=0 #Commun
+        self.eps_min=0.05 #Commun
         self.learning_rate=0.1
         self.discountFactor=0.90
 
@@ -68,8 +68,8 @@ class deep_q_learning(agent):
          self.inputs_dim=inputs_dim
          self.memory_size=memory_size
          self.memory = ReplayBuffer(self.memory_size,inputs_dim)
-         #self.q_eval = dq_network(len(self.actions),inputs_dim)
-         self.q_eval = self.readModel()
+         self.q_eval = dq_network(len(self.actions),inputs_dim)
+         #self.q_eval = self.readModel()
 
     def storeTransition(self,currentState,currentAction,reward,nextState,done):
         self.memory.store_transition(currentState,currentAction,reward,nextState,done)
